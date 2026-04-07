a31e652da40c7269bc2be0bacbf1ba67c85b4750
DESKTOP-FRBKB15\Eric <29125084+tlgj@users.noreply.github.com>
2026-04-07 15:40:05 +0800

    ``` chore(image-helper): 更新版本号至1.14.3
    - 将脚本版本从1.14.2更新到1.14.3
    ```

--- image-helper.user.js
+++ image-helper.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   图片助手
 // @name:en      Image Helper
 // @namespace    https://github.com/tlgj/Browser-Scripts
-// @version      1.14.2
+// @version      1.14.3
 // @description  提取页面图片并清洗到高清，支持多品牌 URL 规则、幻灯片浏览、独立查看器、保存/快速保存/全部保存，并支持脚本黑名单。
 // @author       tlgj
 // @license      MIT
