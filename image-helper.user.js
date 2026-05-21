329350c19a38179cbe6e9dafd41a13b3e36624e0
DESKTOP-FRBKB15\Eric <29125084+tlgj@users.noreply.github.com>
2026-05-21 23:45:05 +0800

    up

--- image-helper.user.js
+++ image-helper.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   图片助手
 // @name:en      Image Helper
 // @namespace    https://github.com/tlgj/Browser-Scripts
-// @version      1.17.2
+// @version      1.17.3
 // @description  提取页面图片并清洗到高清，支持多品牌 URL 规则、幻灯片浏览、独立查看器、保存/快速保存/全部保存，并支持脚本黑名单。
 // @author       tlgj
 // @license      MIT
@@ -3580,8 +3580,8 @@
       toggleSlideshow();
     });
 
-    applyBtnPosition(btn);
     document.body.appendChild(btn);
+    applyBtnPosition(btn);
   }
 
   const PANEL_ID = "tm-slide-settings-simple";
